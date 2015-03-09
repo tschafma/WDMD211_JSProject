f8ba8ff262b98ccf404f2626c49c874722443245
tschafma <tscha316@uwsp.edu>
2015-03-08 21:21:04 -0500

    Example artists/albums

---
+++ examples/exampleJSON.js
@@ -0,0 +1,86 @@
+var library = {"albums": [
+				{
+				 "artist": 	"Bjork",
+				 "album":	"Vespertine",
+				},
+				{
+				 "artist": "Bjork",
+				 "album": "Biophilia"
+				},
+				{
+				 "artist": "Fleet Foxes",
+				 "album": "Helplessness Blues",
+				},
+				{
+				 "artist": "Austra",
+				 "album": "Olympia"
+				},
+				{
+				 "artist": "Active Child",
+				 "album": "You Are All I See"
+				},
+				{
+				 "artist": "Arcade Fire",
+				 "album": "Reflektor"
+				},
+				{
+				 "artist": "Tycho",
+				 "album": "Dive"
+				},
+				{
+				 "artist": "Nosaj Thing",
+				 "album": "Drift"
+				},
+				{
+				 "artist": "Clark",
+				 "album": "Iradelphic"
+				},
+				{
+				 "artist": "Grimes",
+				 "album": "Visions"
+				},
+				{
+				 "artist": "LCD Soundsystem",
+				 "album": "This Is Happening"
+				},
+				{
+				 "artist": "Snowmine",
+				 "album": "Dialects"
+				},
+				{
+				 "artist": "Phantogram",
+				 "album": "Eyelid Movies",
+				},
+				{
+				 "artist": "Panda Bear",
+				 "album": "Panda Bear Meets The Grim Reaper",	
+				},
+				{
+				 "artist": "Animal Collective",
+				 "album": "Merriweather Post Pavilion"
+				},
+				{
+				 "artist": "Lorde",
+				 "album": "Pure Heroine",
+				},
+				{
+				 "artist": "CHVRCHES",
+				 "album": "The Bones Of What You Believe"
+				},
+				{
+				 "artist": "The Knife",
+				 "album": "Shaking The Habitual"
+				},
+				{
+				 "artist": "Florence + The Machine",
+				 "album": "Ceremonials"
+				},
+				{
+				 "artist": "Iamamiwhoami",
+				 "album": "Blue",
+				}
+				/*{
+					"artist": "blah",
+					"album": "blah"
+				}*/
+			]}
